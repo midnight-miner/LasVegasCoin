7e020c302a02c8bf9e73554e7f3e18b03339dc81
Kees de Wit <keesdewit@keesdewit.nl>
2017-07-13 22:43:30 +0200

    Update example address

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -110,7 +110,7 @@ void setupAddressWidget(QValidatedLineEdit* widget, QWidget* parent)
 #if QT_VERSION >= 0x040700
     // We don't want translators to use own addresses in translations
     // and this is the only place, where this address is supplied.
-    widget->setPlaceholderText(QObject::tr("Enter a Las Vegas Coin address (e.g. %1)").arg("D7VFR83SQbiezrW72hjcWJtcfip5krte2Z"));
+    widget->setPlaceholderText(QObject::tr("Enter a Las Vegas Coin address (e.g. %1)").arg("VGS7R83SQbiezrW72hjcWJtcfip5krte2Z"));
 #endif
     widget->setValidator(new BitcoinAddressEntryValidator(parent));
     widget->setCheckValidator(new BitcoinAddressCheckValidator(parent));
